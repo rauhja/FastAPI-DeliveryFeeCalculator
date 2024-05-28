219796b1840a56e5e09d6bc668ad65993442e73f
rauhja <82282638+rauhja@users.noreply.github.com>
2024-05-28 14:51:58 +0300

    Add type safety to feecalculator

--- app/feecalculator.py
+++ app/feecalculator.py
@@ -1,10 +1,17 @@
 import math
+from datetime import datetime
 
 from .constants import Constants
 
 
 class FeeCalculator:
-    def __init__(self, cart_value, delivery_distance, number_of_items, time) -> None:
+    def __init__(
+        self,
+        cart_value: int,
+        delivery_distance: int,
+        number_of_items: int,
+        time: datetime,
+    ) -> None:
         self.cart_value = cart_value
         self.delivery_distance = delivery_distance
         self.number_of_items = number_of_items
@@ -13,10 +20,10 @@ class FeeCalculator:
         self.rush_hour = Constants.RUSH_HOUR
         self.calculate_delivery_fee()
 
-    def get_delivery_fee(self):
+    def get_delivery_fee(self) -> int:
         return self.delivery_fee
 
-    def calculate_delivery_fee(self):
+    def calculate_delivery_fee(self) -> None:
         self.calculate_small_order_fee()
         self.calculate_delivery_fee_by_distance()
         self.calculate_extra_item_fee()
@@ -24,10 +31,10 @@ class FeeCalculator:
         self.check_if_over_max_delivery_fee()
         self.check_if_eligible_for_free_delivery()
 
-    def calculate_small_order_fee(self):
+    def calculate_small_order_fee(self) -> None:
         self.delivery_fee += max(Constants.MIN_ORDER_VALUE - self.cart_value, 0)
 
-    def calculate_delivery_fee_by_distance(self):
+    def calculate_delivery_fee_by_distance(self) -> None:
         extra_distance = max(self.delivery_distance - Constants.BASE_DISTANCE, 0)
         extra_distance_fee = (
             int(math.ceil(extra_distance / Constants.ADDITIONAL_DISTANCE))
@@ -35,23 +42,23 @@ class FeeCalculator:
         )
         self.delivery_fee += extra_distance_fee
 
-    def calculate_extra_item_fee(self):
+    def calculate_extra_item_fee(self) -> None:
         extra_items = max(self.number_of_items - Constants.ITEMS_WITHOUT_SURCHARGE, 0)
         self.delivery_fee += extra_items * Constants.EXTRA_ITEM_FEE
         if self.number_of_items > Constants.ITEMS_COUNT_FOR_BULK_FEE:
             self.delivery_fee += Constants.EXTRA_BULK_FEE
 
-    def calculate_rush_hour_fee(self):
+    def calculate_rush_hour_fee(self) -> None:
         for key, value in self.rush_hour.items():
             if self.time.isoweekday() == key and self.time.hour in value:
                 self.delivery_fee = int(
                     self.delivery_fee * Constants.RUSH_HOUR_MULTIPLIER
                 )
 
-    def check_if_over_max_delivery_fee(self):
+    def check_if_over_max_delivery_fee(self) -> None:
         if self.delivery_fee > Constants.MAX_DELIVERY_FEE:
             self.delivery_fee = Constants.MAX_DELIVERY_FEE
 
-    def check_if_eligible_for_free_delivery(self):
+    def check_if_eligible_for_free_delivery(self) -> None:
         if self.cart_value >= Constants.FREE_DELIVERY_CART_VALUE:
             self.delivery_fee = 0
